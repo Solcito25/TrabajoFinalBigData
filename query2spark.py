a2ec62d47f2048c73ba31da81f77ba39769435fe
Solcito25 <51237026+Solcito25@users.noreply.github.com>
2023-06-10 18:50:58 -0500

    queries spark

---
+++ query2spark.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import IntegerType
+
+spark = SparkSession.builder \
+    .appName("311 Analysis") \
+    .config("spark.executor.memory", "3g") \
+    .config("spark.driver.memory", "3g") \
+    .getOrCreate()
+
+df = spark.read.csv("s3://clustertuplexproofs231/311_subset.csv", header=True, inferSchema=True)
+
+year_to_investigate = 2019
+
+def extract_month(date):
+    date = date.split(" ")[0]
+    return int(date.split("/")[0])
+
+def extract_year(date):
+    date = date.split(" ")[0]
+    return int(date.split("/")[-1])
+
+extract_month_udf = udf(extract_month, IntegerType())
+extract_year_udf = udf(extract_year, IntegerType())
+
+df2 = df.withColumn("Month", extract_month_udf(col("Created Date"))) \
+    .withColumn("Year", extract_year_udf(col("Created Date"))) \
+    .filter(col("Complaint Type").contains("Mosquito")) \
+    .filter(col("Year") == year_to_investigate) \
+    .select("Month", "Year", "Complaint Type")
+
+df2.groupBy("Month") \
+    .count() \
+    .show()
